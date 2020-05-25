be15068bb7427c1678524d6186878d99ae0e0f0a
yuru-camper <daichan90401@gmail.com>
2020-05-25 18:03:19 +0900

    ログインページを作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,41 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        playground
-      </h1>
-      <h2 class="subtitle">
-        My smashing Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <h3>sign in</h3>
+    <div class="mail_address">
+      <h4>Mail Address</h4>
+      <input type="text" id="mail_address" v-model="mail_address">
     </div>
+    <div class="password">
+      <h4>Password</h4>
+      <input type="text" id="password" v-model="password">
+    </div>
+    <p class="error_message"></p>
+    <button @click="login">sign in</button>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import firebase from '~/plugins/firebase.js'
 
 export default {
-  components: {
-    Logo
+  data() {
+    return {
+      mail_address: '',
+      password: '',
+      valid: false,
+      valid_error: 'UserIDまたはPasswordが間違っています'
+    }
+  },
+  methods: {
+    login() {
+      firebase.auth().signInWithEmailAndPassword(this.mail_address, this.password)
+        .then(user => {
+          this.$store.commit('login_state', this.mail_address, this.password)
+          this.$router.push('top')
+        }).catch((error) => {
+          this.valid = true;
+        })
+    }
   }
 }
 </script>
